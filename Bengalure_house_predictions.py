5fc4dc88cfbc15ed78cd9fac732c2864dcc8fa93
Saravanan Sevanan <saravanansevanan@gmail.com>
2024-01-02 20:36:10 +0530

    # Predicting home prices in Bengaluru
    ---This data science project walks through step by step process of how to build a real estate price prediction.
    ---The project covers most of data science concepts such as data load and cleaning, outlier detection and removal, 
        feature engineering, dimensionality reduction, gridsearchcv for hyperparameter tunning, etc.
    ---Technology and tools wise this project covers,
    	1) Python
    	2) Numpy and Pandas for data cleaning
    	3) Matplotlib for data visualization
    	4) Sklearn for model building
    	5) Jupyter notebook as IDE

---
+++ Bengalure_house_predictions.py
@@ -0,0 +1,8 @@
+#Bangaluru_House_Predictions
+#Steps 1 : Import the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#step 2:Import the dataset and create a dataframe
+ 
